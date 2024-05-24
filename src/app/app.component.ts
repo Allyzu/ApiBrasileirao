a1e5e7473f9ca54a0a343e7bb449af38d69c64b0
Allyzu <henriqueallyson18@gmail.com>
2024-05-24 18:54:15 -0300

    "Update: Removed the template, css and logic from app.component to be relocated to the component table-component folder"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CampeonatoService } from './services/campeonato.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,10 @@ import { CampeonatoService } from './services/campeonato.service';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  //Criando objeto tabela
-  tabela!:any;
-
-  //Referenciando a pasta campeonato.service para o uso do método get
-  constructor(private campeonatoService:CampeonatoService){}
-
-  //Inicio usando o objeto tabela para receber os dados ja observados da requisição feita com método get na pasta service
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
   ngOnInit(): void {
-    this.tabela = this.campeonatoService.getTabelaSerieB();
+    throw new Error('Method not implemented.');
   }
 }
