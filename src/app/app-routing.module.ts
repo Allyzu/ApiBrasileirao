6156e1867ed504dc8e564b27812568bce3226b3e
Allyzu <henriqueallyson18@gmail.com>
2024-05-24 18:50:45 -0300

    "Update: Update of the route to pass the position of the chosen team in the url"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TimeComponentComponent } from './components/time-component/time-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'time/:posicao', component: TimeComponentComponent }
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
